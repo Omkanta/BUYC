e2e1edf688776fed1eca9e09bd192ed48d440729
Om kanta Prasad Yadav <112754465+Omkanta@users.noreply.github.com>
2023-07-19 13:17:37 +0530

    User authentication & authorization added

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express=require("express");
 const { connection } = require("./db");
 require("dotenv").config();
+const {userRouter} =require("./routes/user.routes");
 
 const app=express();
 app.use(express.json());
 
+app.use("/users",userRouter);
 
 
 
